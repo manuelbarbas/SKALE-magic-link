af93af745cbb6c172dc06a2fe3207fc2aa835fa2
manuelbarbas <manuel@skalelabs.com>
2024-02-09 13:45:51 +0000

    web3 + ethers tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Magic } from 'magic-sdk';
 import { ethers } from "ethers";
+import Web3 from "web3";
 
-
-const customNodeOptions_chaos = {
-  rpcUrl: 'https://staging-v3.skalenodes.com/v1/staging-fast-active-bellatrix',
-  chainId: 1351057110
-}
 
 const customNodeOptions_nebula = {
-  rpcUrl: 'https://staging-v3.skalenodes.com/v1/staging-faint-slimy-achird',
-  chainId: 503129905
+  rpcUrl: 'https://testnet.skalenodes.com/v1/lanky-ill-funny-testnet',
+  chainId: 37084624
 }
 
-
-const customNodeOptions_polygon = {
-  rpcUrl: 'https://polygon-mumbai.infura.io/v3/d3fc3c62fac246f885f4f6af21497f2e',
-  chainId: 80001
-}
-
-
-async function Test() {
+async function LoginAndSendGas() {
   try {
     let magic;
 
@@ -30,12 +19,57 @@ async function Test() {
       network: customNodeOptions_nebula,
     });
 
-  await magic.auth.loginWithMagicLink({ email: 'manuel@skalelabs.com', showUI:false });
+   // const provider = new ethers.BrowserProvider(magic.rpcProvider);
+   const web3 = new Web3(magic.rpcProvider);
 
-  const accounts = await magic.wallet.connectWithUI();
 
-  console.log(accounts);
+    await magic.auth.loginWithMagicLink({ email: 'manuel@skalelabs.com'});
 
+    const account = await web3.eth.getAccounts();
+
+    console.log(account);
+
+    const txnParams = {
+      from: account[0],
+      to: "0x8d18146ae92a42b7fc3D8511CBc54a1addaE81e0",
+      value: web3.utils.toWei('0.000001', "ether"),
+      gasLimit: 510000,
+      gasPrice: 100000
+  };
+  
+  web3.eth
+      .sendTransaction(txnParams)
+      .on("transactionHash", (hash) => {
+          console.log("Transaction hash:", hash);
+      })
+      .then((receipt) => {
+          console.log("Transaction receipt:", receipt);
+      })
+      .catch((error) => {
+          console.error(error);
+      });
+
+   /* const signer = await provider.getSigner();
+
+    console.log("ADDRESS SIGNER "  + signer.address)
+
+    const signerBalance =  await provider.getBalance(signer.address); 
+    console.log("sFUEL Balance " + ethers.formatEther(signerBalance) + " sFUEL");
+  
+
+    const txnParams = {
+      to: "0x8d18146ae92a42b7fc3D8511CBc54a1addaE81e0",
+      value: ethers.parseUnits('0.000001', 'ether'),
+      gasLimit: ethers.parseUnits('0.0001', 'gwei'),
+      gasPrice: ethers.parseUnits('0.0001', 'gwei'),
+    };
+
+    const tx = await signer.sendTransaction(txnParams);
+
+    console.log("TX HASH " + tx.hash)
+
+    const signerBalance_ =  await provider.getBalance(signer.address); 
+    console.log("UPDATED sFUEL Balance " + ethers.formatEther(signerBalance_) + " sFUEL");*/
 
 } catch(err) {
   console.log(err);
@@ -45,11 +79,10 @@ async function Test() {
 function App() {
 
 
-
   return (
     <div className="App">
       <header className="App-header">
-      <button onClick={() => Test()}>Login</button>
+      <button onClick={() => LoginAndSendGas()}>Login</button>
       </header>
     </div>
   );
